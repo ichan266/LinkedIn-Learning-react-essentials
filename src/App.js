69ef1cecba10dc712d0a3629d09aa3182b2afca5
Iris Kuhn <ichan266@hotmail.com>
2021-04-07 17:25:18 -0700

    Chapter 3 - Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function Header() {
+  return (
+    <header>
+      <h1>Iris' Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We server the most delicious food here!</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's true!</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import React from "react";
+// import reactDom from "react-dom";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-    <li>Thursday</li>
-    <li>Friday</li>
-  </ul>,
-  document.getElementById("root")
+  // React.createElement("h1", { style: { color: "rebeccapurple" } }, "Wednesday"),
+  // <ul>
+  //   <li>Monday</li>
+  //   <li>Tuesday</li>
+  //   <li>Wednesday</li>
+  //   <li>Thursday</li>
+  //   <li>Friday</li>
+  // </ul>,
+  <App />,
+  document.querySelector("#root")
 );
+
+//% ReactDom.render takes in 2 arguments.
+//% 1st argument takes in an HTML element or a function that return HTML.
+//! It does not run in the browser natively. Taking in HTML like that is made possible by Babel
+//@   1. a React Function
+//@   2. React.createElement (See line 9)
+//@   3. straight HTML
+//% 2nd argument takes in a document method that target a specific DOM
+//@   eg. document.querySelector("#root")
+
+//% React.CreateElement takes 3 arguments here:
+//% 1st argument is an HTML tag as a string, eg. "h1"
+//% 2nd argument is CSS (if no styling needed, pass in `null`)
+//% 3rd argument is the content inside the HTML element. You can also use React.CreateElement() as the 3rd argument for nesting elements
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
