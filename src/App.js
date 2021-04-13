c57f5110268c3cfbdd3899bc995050bef5748971
Iris Kuhn <ichan266@hotmail.com>
2021-04-13 12:25:12 -0700

    Chapter 4 - useReducer

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
-import React, { useState, useEffect } from "react";
+// Example before using useReducer
+// import React, { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [checked, setChecked] = useState(false);
+//   function toggle() {
+//     setChecked((checked) => !checked);
+//   }
+//   return (
+//     <>
+//       <input type="checkbox" value={checked} onChange={toggle} />
+//       <p>{checked ? "checked" : "not checked"}</p>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React, { useReducer } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here!`);
-  }, [secondary]);
-
+  const [checked, toggle] = useReducer((checked) => !checked, false);
   return (
     <>
-      <h1>
-        Current emotion is {emotion} and {secondary}.
-      </h1>
-      <button onClick={() => setEmotion("happy")}>Make Happy</button>
-      <button onClick={() => setSecondary("crabby")}>Make Crabby</button>
-      <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-      <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
-      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   );
 }
 
 export default App;
-
-// % {props.authorized ? <SecretComponent /> : <RegularComponent />}
-// %   This whole statement means:
-// %    1. If props.authorized is true, (i.e. ? means if), then render <SecretComponent />
-// %    2. Else, render <RegularComponent /> (i.e. : means else)
-// %
-// %
-// %
-// % function App({ authorized }) {
-// %    return <>{authorized ? <SecretComponent /> : <RegularComponent />}</>;
-// % }
-// %   Using App({ authorized }) this way (instead of prop.authorized) is called object constructing
-// %
-// %
-// %
-// %
